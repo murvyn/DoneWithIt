6ec7109deb8bf2d058a25fb8fec677cf39857d10
Murvyn <99991623+murvyn@users.noreply.github.com>
2024-07-28 01:15:53 +0000

    initial commit

--- App.js
+++ App.js
@@ -1,20 +1,71 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+// import { StatusBar } from "expo-status-bar";
+import {
+  Image,
+  SafeAreaView,
+  StyleSheet,
+  Text,
+  View,
+  TouchableOpacity,
+  TouchableNativeFeedback,
+  Button,
+  Alert,
+  Platform,
+  StatusBar,
+  Dimensions,
+} from "react-native";
+import {
+  useImageDimensions,
+  useDeviceOrientation,
+} from "@react-native-community/hooks";
+import WelcomeScreen from "./app/screens/WelcomeScreen";
 
 export default function App() {
+  // const handlePress = () =>{
+  //   // Alert.alert("My title", "My message", [
+  //   //   { text: "Yes", onPress: () => console.log("yes") },
+  //   //   { text: "No", onPress: () => console.log("no") },
+  //   // ]);
+  //   Alert.prompt("my title", "my message", text=> console.log(text))
+  // }
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      {/* <Text numberOfLines={1} onPress={handlePress}>
+        Hello React Native
+      </Text>
+      <TouchableNativeFeedback onPress={handlePress}>
+        <View style={{width: 200, height: 70, backgroundColor: "dodgerblue"}}></View>
+        <Image
+          blurRadius={10}
+          fadeDuration={1000}
+          source={{
+            width: 200,
+            height: 300,
+            uri: "https://picsum.photos/200/300",
+          }}
+        />
+      </TouchableNativeFeedback> */}
+      {/* <StatusBar style="auto" /> */}
+      {/* <Button color="orange" title="Click Me" onPress={handlePress} /> */}
+
+      {/* <View 
+       style={{backgroundColor: "dodgerblue", width: '100%', height: useDeviceOrientation() === "landscape" ? "100%" : '30%'}}></View> */}
+
+      {/* <View style={{ backgroundColor: "#fff", flex: 1, flexDirection: "row", justifyContent: "center", alignItems: 'center' }}>
+        <View style={{ backgroundColor: "dodgerblue", width: 100, height: 300 }} />
+        <View style={{ backgroundColor: "gold", width: 100, height: 200 }} />
+        <View style={{ backgroundColor: "tomato", width: 100, height: 100 }} />
+      </View> */}
+      <WelcomeScreen />
+    </SafeAreaView>
   );
 }
+
+const containerStyle = {};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0,
   },
 });
